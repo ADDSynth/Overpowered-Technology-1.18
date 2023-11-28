b8e218aa842b5cfae80a998b1f3e222c557b0d5c
ADDSynth <addsynth@gmail.com>
2023-11-28 09:34:30 -0600

    Fix CircuitFabricator Filter/Recipe Not Being Loaded on Dedicated Servers

--- src/main/java/addsynth/energy/ADDSynthEnergy.java
+++ src/main/java/addsynth/energy/ADDSynthEnergy.java
@@ -29,6 +29,7 @@ import net.minecraft.world.item.CreativeModeTab;
 import net.minecraft.world.item.ItemStack;
 import net.minecraft.world.item.crafting.RecipeManager;
 import net.minecraftforge.common.MinecraftForge;
+import net.minecraftforge.event.server.ServerAboutToStartEvent;
 import net.minecraftforge.event.server.ServerStartedEvent;
 import net.minecraftforge.eventbus.api.IEventBus;
 import net.minecraftforge.fml.ModLoadingContext;
@@ -63,6 +64,7 @@ public class ADDSynthEnergy {
     bus.addListener(ADDSynthEnergy::main_setup);
     bus.addListener(ADDSynthEnergyCompat::sendIMCMessages);
     bus.addListener(ADDSynthEnergy::client_setup);
+    MinecraftForge.EVENT_BUS.addListener(ADDSynthEnergy::onServerAboutToStart);
     MinecraftForge.EVENT_BUS.addListener(ADDSynthEnergy::onServerStarted);
     init_config();
   }
@@ -81,7 +83,7 @@ public class ADDSynthEnergy {
     CircuitFabricatorRecipes.INSTANCE.register();
   }
 
-  public static void onServerStarted(final ServerStartedEvent event){
+  public static void onServerAboutToStart(final ServerAboutToStartEvent event){
     @SuppressWarnings("resource")
     final MinecraftServer server = event.getServer();
     
@@ -89,7 +91,9 @@ public class ADDSynthEnergy {
     final RecipeManager recipe_manager = server.getRecipeManager();
     CompressorRecipes.INSTANCE.rebuild(recipe_manager);
     CircuitFabricatorRecipes.INSTANCE.rebuild(recipe_manager);
-    
+  }
+
+  public static void onServerStarted(final ServerStartedEvent event){
     if(Compatibility.PROJECT_E.loaded){
       if(DEV_STAGE.isDevelopment){
         EMCValue.check_items(MOD_ID);

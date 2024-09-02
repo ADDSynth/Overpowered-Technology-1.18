105fc633951ff124222cbda4be740c0d258c2120
ADDSynth <addsynth@gmail.com>
2024-09-02 12:03:52 -0500

    Various Minor Changes

--- src/main/java/addsynth/core/gameplay/team_manager/data/TeamDataUnit.java
+++ src/main/java/addsynth/core/gameplay/team_manager/data/TeamDataUnit.java
@@ -32,8 +32,8 @@ public final class TeamDataUnit {
     data.writeBoolean(see_invisible_allys);
     data.writeByte(nametag_option);
     data.writeByte(death_message_option);
-    data.writeUtf(prefix.getString());
-    data.writeUtf(suffix.getString());
+    data.writeComponent(prefix);
+    data.writeComponent(suffix);
     int i;
     final int length = players.size();
     final TextComponent[] player_names = new TextComponent[length];
@@ -52,8 +52,8 @@ public final class TeamDataUnit {
     team.see_invisible_allys = data.readBoolean();
     team.nametag_option = data.readByte();
     team.death_message_option = data.readByte();
-    team.prefix = new TextComponent(data.readUtf());
-    team.suffix = new TextComponent(data.readUtf());
+    team.prefix = data.readComponent();
+    team.suffix = data.readComponent();
     team.players = new ArrayList<Component>();
     for(final Component t : NetworkUtil.readTextComponentArray(data)){
       team.players.add(t);
